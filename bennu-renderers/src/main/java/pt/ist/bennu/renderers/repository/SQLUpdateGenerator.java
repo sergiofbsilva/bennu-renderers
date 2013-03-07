48fc1435a38700e099996c57938fc74e2e595796
pedrosan7os <pedro.san7os@gmail.com>
2013-03-07 23:54:26 +0000

    Removed SQLUpdateGenerator

--- bennu-renderers/src/main/java/pt/ist/bennu/renderers/repository/SQLUpdateGenerator.java
+++
@@ -1,7 +0,0 @@
-package pt.ist.bennu.renderers.repository;
-
-public class SQLUpdateGenerator {
-    public static void main(String[] args) throws Exception {
-        pt.ist.fenixframework.pstm.repository.SQLUpdateGenerator.main(args);
-    }
-}
